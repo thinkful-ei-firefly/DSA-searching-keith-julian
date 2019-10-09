4bb8a34e4fb746bd7b692b6a1837f20d93c307d8
Julianb12 <julianb1212@gmail.com>
2019-10-09 11:40:47 -0500

    4

---
+++ drills/drills.js
@@ -0,0 +1,17 @@
+
+
+//3 Find a book
+/*
+001-099 
+100-199
+200-299
+...
+given Dewey number and book Title
+binary search using Dewey number on all Dewey numbers
+binary search using authors name within Dewey subsect
+linear search of all books written by author in Dewey subsect
+*/
+
+//4
+// 14,19,15,27,25,79,90,91,89,35
+// 5,6,7,8,9,10,11
